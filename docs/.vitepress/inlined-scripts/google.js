5d1b06d49f926da6ebb154f6dd1722f90c889f0d
KieSun <zx597813039@gmail.com>
2023-01-06 15:55:39 +0800

    chore: hotjar

--- docs/.vitepress/inlined-scripts/google.js
+++ docs/.vitepress/inlined-scripts/google.js
@@ -3,3 +3,12 @@ function gtag() { dataLayer.push(arguments); }
 gtag('js', new Date());
 
 gtag('config', 'G-DZPN2FT3DF');
+
+(function(h,o,t,j,a,r){
+    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+    h._hjSettings={hjid:3272944,hjsv:6};
+    a=o.getElementsByTagName('head')[0];
+    r=o.createElement('script');r.async=1;
+    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+    a.appendChild(r);
+})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
