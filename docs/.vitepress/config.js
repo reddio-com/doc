b6033f7df76c5a417c12e76b742eed4a928e08d7
Qiang Zhou <zhouqiang.cl@gmail.com>
2023-06-30 11:56:14 +0800

    using concepts instead of changelog
    Signed-off-by: Qiang Zhou <zhouqiang.cl@gmail.com>

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -110,7 +110,7 @@ function sidebarService() {
       items: [
         { text: "Overview", link: "/guide/introduction/overview" },
         { text: "Features", link: "/guide/introduction/feature" },
-        { text: "Changelog", link: "/guide/introduction/changelog" },
+        { text: "Concepts", link: "/guide/introduction/concepts" },
       ],
     },
     {
